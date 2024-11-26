a8d2f92674c121750aa8ac0496b61f7468ef90f9
fauqaji <fauqaji@gmail.com>
2024-11-26 10:00:31 +0700

    fix code

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const cron = require("node-cron");
 const app = express();
 
 // Konfigurasi CORS
-app.use(cors({ origin: "http://localhost:3001" }));
-app.use(bodyParser.json());
+app.use(cors({ origin: process.env.URL_CORS }));
 
+app.use(bodyParser.json());
 // Menambahkan middleware untuk parsing URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
 
